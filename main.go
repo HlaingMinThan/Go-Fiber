1d4a29e8bdfebfef0f6d6e25997c3c1cf3ba5054
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-07-04 15:09:49 +0700

    post api

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ type Post struct {
 }
 
 func main() {
+	var post Post
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -20,6 +21,19 @@ func main() {
 		post := Post{
 			Title: "hello",
 			Body:  "world",
+		}
+		return c.Status(fiber.StatusOK).JSON(post)
+	})
+
+	app.Post("/post", func(c *fiber.Ctx) error {
+		body := &Post{}
+		err := c.BodyParser(body)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(err)
+		}
+		post = Post{
+			Title: body.Title,
+			Body:  body.Body,
 		}
 		return c.Status(fiber.StatusOK).JSON(post)
 	})
