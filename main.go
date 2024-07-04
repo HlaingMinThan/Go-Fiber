a93efd041832e20f2fe2fca9aa0228dbf36773b9
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-07-04 14:56:03 +0700

    get a post object

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Post struct {
+	Title string
+	Body  string
+}
 
 func main() {
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("HEllO")
+	})
+
+	app.Get("/post", func(c *fiber.Ctx) error {
+		post := Post{
+			Title: "hello",
+			Body:  "world",
+		}
+		return c.Status(fiber.StatusOK).JSON(post)
+	})
 	app.Listen(":3000")
 }
